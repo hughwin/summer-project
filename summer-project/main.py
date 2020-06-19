433b26d0ed010015bc27dbc054673ff2d8b05920
Hugh Winchester <hugh.winchester@gmail.com>
2020-06-19 13:03:23 +0100

    Basic JSON parsing

--- summer-project/main.py
+++ summer-project/main.py
@@ -5,9 +5,9 @@ import json
 def main():
     print("Starting")
     r = requests.get("https://hostux.social/api/v1/timelines/public?limit=5")
-    statuses = json.dumps(r.text)
-    print(statuses)
-
+    statuses = r.json()
+    print(statuses[0])
+    print(statuses[1])
 
 if __name__ == "__main__":
     main()
