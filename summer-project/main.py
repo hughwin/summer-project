eb5b247ab0a041b964f4cefa35a58d2e3afb6586
Hugh Winchester <hugh.winchester@gmail.com>
2020-06-19 12:15:26 +0100

    Basic GET cat request

--- summer-project/main.py
+++ summer-project/main.py
@@ -1,11 +1,12 @@
 import requests
+import json
 
 
 def main():
     print("Starting")
-    r = requests.get("https://xkcd.com/1906/")
-    print(r)
-    print("Finished")
+    r = requests.get("https://hostux.social/api/v1/timelines/public?limit=5")
+    statuses = json.dumps(r.text)
+    print(statuses)
 
 
 if __name__ == "__main__":
