7f769e744db41f486dc726c2f7b0c7381704e174
hughwin <hugh.winchester@gmail.com>
2020-08-04 19:07:16 +0100

    Changed TESSDATA_PREFIX

--- src/bot.py
+++ src/bot.py
@@ -266,7 +266,7 @@ def display_colour_channel(reply, colour):
 
 def get_text_from_image(reply):
     for image in range(len(reply.media)):
-        pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_PREFIX")
+        pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSDATA_PREFIX")
         img = cv2.imread(settings.JPEG_INPUT.format(image))
         text = pytesseract.image_to_string(img)
         return text
