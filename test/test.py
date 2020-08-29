a71f744d0b67938ca06a65fb7c1c09602e365c2a
hughwin <hugh.winchester@gmail.com>
2020-08-29 15:14:54 +0100

    Completed test for get_information_about_image

--- test/test.py
+++ test/test.py
@@ -1,11 +1,16 @@
+from pathlib import Path
 from unittest import TestCase
 
+from dotenv import load_dotenv
+
 import bot
+
+load_dotenv()
 
 
 class TestSpamDefender(TestCase):
 
-    def test_add_user_to_requests(self):
+    def test_add_user_to_requests_test(self):
         spam_defender = bot.SpamDefender()
         assert len(spam_defender.users_who_have_made_requests) == 0
         spam_defender.add_user_to_requests(1)
@@ -20,3 +25,28 @@ class TestSpamDefender(TestCase):
         spam_defender.add_user_to_requests(1)
         spam_defender.add_user_to_requests(1)
         assert spam_defender.allow_account_to_make_request(1) is False
+
+
+class TestBot(TestCase):
+    def test_get_information_about_image_test(self):
+        number_of_pixels = 1102500
+        expected_x = 700
+        expected_y = 525
+
+        expected_string = ("Image properties: "
+                           "\n- Number of Pixels: " + "{}"
+                           + "\n- Shape/Dimensions: " + "({}, " + "{}, " + "{})").format(number_of_pixels,
+                                                                                         expected_y, expected_x, 3)
+        result_string = bot.get_information_about_image(str(Path.cwd() /
+                                                            "test_resources" / "fibonacciSpiralALOE.jpeg"))
+        assert result_string == expected_string
+
+# class TestMastodon(TestCase):
+#
+#     def test_mastodon_notfications(self):
+#         mastodon = Mastodon(
+#             access_token=os.getenv("ACCESS_TOKEN"),
+#             api_base_url=settings.BASE_ADDRESS
+#         )
+#         notifications = mastodon.notifications()
+#         print(notifications)
