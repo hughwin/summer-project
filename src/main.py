613024c2436e68e0d50fe0b2ce01869a1fd46a80
hughwin <hugh.winchester@gmail.com>
2020-09-09 23:29:23 +0100

    Test google vision

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-import os
-
 import bot
-import settings
 
 
 def main():
-    with open(str(settings.BASE_DIRECTORY / "GCP_CRED.json"), "w") as file:
-        file.write(os.getenv("GCP_CRED"))
+    # with open(str(settings.BASE_DIRECTORY / "GCP_CRED.json"), "w") as file:
+    #     file.write(os.getenv("GCP_CRED"))
     bot.start_bot()
 
 
