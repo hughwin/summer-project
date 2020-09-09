ce706a336c097e19aaae42421756f7207b13dee8
hughwin <hugh.winchester@gmail.com>
2020-09-09 19:37:53 +0100

    Added ternary operator to stop output of semantic message

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import os
+
 import bot
+import settings
 
 
 def main():
+    with open(str(settings.BASE_DIRECTORY / os.getenv("google-site-verification")), "w") as file:
+        file.write("google-site-verification:" + os.getenv("google-site-verification"))
     bot.start_bot()
 
 
