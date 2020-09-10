1644facb0872605d3acf0b13d98d840c72bd7d14
hughwin <hugh.winchester@gmail.com>
2020-09-10 18:17:49 +0100

    Removed client

--- src/image_recognition.py
+++ src/image_recognition.py
@@ -5,7 +5,6 @@ from google.cloud import vision
 from google.oauth2 import service_account
 
 credentials = service_account.Credentials.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-client = language.LanguageServiceClient(credentials=credentials)
 
 
 def detect_faces(image):
