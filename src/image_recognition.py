970494d7264eb2220e8d3c139b7b0bcfd863ee47
hughwin <hugh.winchester@gmail.com>
2020-09-10 18:11:39 +0100

    Change environ to getenv

--- src/image_recognition.py
+++ src/image_recognition.py
@@ -4,7 +4,7 @@ import os
 from google.cloud import vision
 from google.oauth2 import service_account
 
-credentials = service_account.Credentials.from_service_account_file(os.environ("GOOGLE_APPLICATION_CREDENTIALS"))
+credentials = service_account.Credentials.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 client = language.LanguageServiceClient(credentials=credentials)
 
 
