7ecd03cdf50b2cb4464c11f688c2133f66a99db6
hughwin <hugh.winchester@gmail.com>
2020-09-11 11:40:44 +0100

    Hide private keys

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import os
+
 import bot
+import settings
 
 
 def main():
-    # with open(str(settings.BASE_DIRECTORY / "GCP_CRED.json"), "w") as file:
-    #     file.write(os.getenv("GCP_CRED"))
+    with open(str(settings.BASE_DIRECTORY / "GCP_CRED.json"), "w") as file:
+        file.write(os.getenv("GCP_CRED"))
     bot.start_bot()
 
 
