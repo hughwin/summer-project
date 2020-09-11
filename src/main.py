c54a9d2d5da9ae88e9037b2b3778895e1cd79119
hughwin <hugh.winchester@gmail.com>
2020-09-11 13:03:11 +0100

    json loads instead of dumps

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import settings
 
 def main():
     with open(str(settings.BASE_DIRECTORY / "gcp_cred.json"), "w") as file:
-        os.getenv("GCP_CRED", file)
+        json.loads(os.getenv("GCP_CRED"), file)
     with open(str(settings.BASE_DIRECTORY / 'gcp_cred.json'), 'r') as f:
         data = json.load(f)
     print(data)
