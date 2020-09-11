9cde2a5c3bacc1ccaf61facf716da05fa221369b
hughwin <hugh.winchester@gmail.com>
2020-09-11 11:45:03 +0100

    Lowercase gcp_cred.json

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import settings
 
 
 def main():
-    with open(str(settings.BASE_DIRECTORY / "GCP_CRED.json"), "w") as file:
+    with open(str(settings.BASE_DIRECTORY / "gcp_cred.json"), "w") as file:
         file.write(os.getenv("GCP_CRED"))
     bot.start_bot()
 
